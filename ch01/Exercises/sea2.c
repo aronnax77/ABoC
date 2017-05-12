f84878d37d631ed47ea2050fb628c5231043947f
Richard Myatt <lydgate77@hotmail.co.uk>
2017-05-12 19:53:30 +0100

    Completed exercise 08, started exercise 09

---
+++ ch01/Exercises/sea2.c
@@ -0,0 +1,16 @@
+// file: sea2.c
+// page 10
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\n\n\n\n\n\n\n\n\n\n");
+	printf("            *****************************\n");
+	printf("            *    from sea               *\n");
+	printf("            *    to shining C           *\n");
+	printf("            *****************************\n");
+	printf("\n\n\n\n\n\n\n\n\n\n");
+
+	return 0;
+}
