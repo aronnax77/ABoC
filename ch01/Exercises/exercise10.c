0048bfaebb19f2adab76683249368b6d8051d86a
Richard Myatt <lydgate77@hotmail.co.uk>
2017-05-15 19:37:26 +0100

    Exercise 10 now completed

--- ch01/Exercises/exercise10.c
+++ ch01/Exercises/exercise10.c
@@ -22,3 +22,50 @@ make: *** [nonesense] Error 1
 tyrion$~: 
 
 */
+
+/* Using the sea2.c programe with the following code:
+
+// file: sea2.c
+// page 10
+
+// #include <stdio.h>
+
+int main(void)
+{
+	printf("\n\n\n\n\n\n\n\n\n\n");
+	printf("            *****************************\n");
+	printf("            *    from sea               *\n");
+	printf("            *    to shining C           *\n");
+	printf("            *****************************\n");
+	printf("\n\n\n\n\n\n\n\n\n\n");
+
+	return 0;
+}
+
+taking our the #include statement as illustrated gave the following error:
+
+tyrion$~: make sea2
+cc     sea2.c   -o sea2
+sea2.c: In function ‘main’:
+sea2.c:8:2: warning: implicit declaration of function ‘printf’ [-Wimplicit-function-declaration]
+  printf("\n\n\n\n\n\n\n\n\n\n");
+  ^
+sea2.c:8:2: warning: incompatible implicit declaration of built-in function ‘printf’
+sea2.c:8:2: note: include ‘<stdio.h>’ or provide a declaration of ‘printf’
+tyrion$~:
+
+taking our the return statement and reinstating the #include does not raise an error or warning.
+
+taking out one of the block brackets and reinstating all else produced the following error
+
+tyrion$~: make sea2
+cc     sea2.c   -o sea2
+sea2.c: In function ‘main’:
+sea2.c:15:2: error: expected declaration or statement at end of input
+  return 0;
+  ^
+<builtin>: recipe for target 'sea2' failed
+make: *** [sea2] Error 1
+tyrion$~:
+
+*/
